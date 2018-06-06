64e9153bc7841b7f719fb6dda6122db619f49f9e
leanfj <leanfj@hotmail.com>
2018-06-06 13:42:38 -0300

    update

--- objects-advanced/script.js
+++ objects-advanced/script.js
@@ -1,3 +1,4 @@
+//Object letral
 let meuObjeto = {
   primeiroNome: 'Leandro',
   segundoNome: 'Ferreira'
@@ -5,15 +6,57 @@ let meuObjeto = {
 
 display(meuObjeto)
 
+//Constructor function
+
 function Pessoa (argNome, argSobrenome) {
-  this.state = {
-    nome: argNome,
-    sobreNome: argSobrenome,
-    nomeCompleto: ''
-  }
-  console.log(this.state);
+  this.nome = argNome;
+  this.sobre = argSobrenome
 }
 
-let leandro = new Pessoa('Leandro', 'Ferreira');
+//Exemplo onde this se referencia ao global object
+let leandro = Pessoa('Leandro', 'Ferreira');
 
-console.log(leandro);
+display(window.nome);
+
+
+//Colocando o this para ser referenciado dentro do object criado com new keyword
+let dayana = new Pessoa('Dayana', 'Brandao');
+
+display(dayana);
+
+display(dayana.nome);
+
+//Criando class ES6
+
+class Cachorro {
+  constructor (argNome, argRaca){
+    this.nome = argNome,
+    this.raca = argRaca
+  }
+
+  //Metodos
+  latir(){
+    console.log('Auau');
+  }
+}
+
+let cachorro1 = new Cachorro('Bolota', 'SRD');
+
+console.log(Cachorro);
+display(cachorro1);
+cachorro1.latir();
+
+let minhaFruta = {
+  nome: 'maça',
+  peso: 100,
+  cor: 'vermelha'
+}
+
+for(let item in minhaFruta){
+  console.log(minhaFruta[item]);
+}
+
+
+console.log(Object.keys(minhaFruta));
+
+
